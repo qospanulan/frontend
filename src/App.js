fda4d8b31a01ea5046a82acb79850ad81eb26b2e
qospanulan <kospanovulan@gmail.com>
2024-02-26 00:55:46 +0600

    token and numbers

--- src/App.js
+++ src/App.js
@@ -15,14 +15,23 @@ import Orders from "./scenes/orders/orderList";
 import CreateOrders from "./scenes/orders/createOrder";
 import CreateBnumberGroup from "./scenes/orders/createBnumberGroup";
 import TCG from "./scenes/tcg";
+import { getCurrentUser } from "./state/api/users/signIn";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const [username, setUsername] = useState();
 
   useEffect(() => {
     const fetchUser = async () => {
       setUser(JSON.parse(localStorage.getItem("user")));
+      if (user?.token) {
+        const response = await getCurrentUser(user.token);
+
+        if (response) {
+          setUsername(response.first_name + " " + response.last_name);
+        }
+      }
     };
 
     fetchUser();
@@ -34,14 +43,14 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <div className="app">
-            {!user && (
+            {!username && (
               <Routes>
                 <Route path="/login" element={<LoginForm />} />
                 <Route path="/logout" element={<LogoutForm />} />
                 <Route path="*" element={<Navigate to="/login" replace />} />
               </Routes>
             )}
-            {user && (
+            {username && (
               <>
                 <Sidebar />
                 <main className="content">
@@ -51,7 +60,9 @@ function App() {
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route
                       path="/login"
-                      element={!user ? <LoginForm /> : <Navigate to={"/"} />}
+                      element={
+                        !username ? <LoginForm /> : <Navigate to={"/"} />
+                      }
                     />
                     <Route path="/logout" element={<LogoutForm />} />
                     <Route path="/" element={<TCG />} />
--- src/scenes/carriers/carrierList.jsx
+++ src/scenes/carriers/carrierList.jsx
@@ -1,9 +1,29 @@
-import { Box, Button, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import {
+  Box,
+  Button,
+  useTheme,
+  Modal,
+  Grid,
+  List,
+  Typography,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { useEffect, useState } from "react";
 import { getCarriersApi } from "../../state/api/carriers";
+
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+
+// function generate(element) {
+//   return [0, 1, 2].map((value) =>
+//     cloneElement(element, {
+//       key: value,
+//     })
+//   );
+// }
 
 const Carriers = () => {
   const theme = useTheme();
@@ -11,6 +31,8 @@ const Carriers = () => {
 
   const [loading, setLoading] = useState(false);
   const [carriers, setCarriers] = useState([]);
+  const [detailId, setDetailId] = useState();
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     async function fetchCarriers() {
@@ -23,6 +45,11 @@ const Carriers = () => {
 
     fetchCarriers();
   }, []);
+
+  const handleDetailClick = function (id) {
+    setDetailId(id);
+    setIsOpen(true);
+  };
 
   const columns = [
     {
@@ -57,16 +84,129 @@ const Carriers = () => {
       flex: 1,
       valueGetter: (params) =>
         params.row.numbers.map((number_info) => ` ${number_info.number}`) || "",
+      renderCell: (params) => {
+        return (
+          <Box display="flex">
+            <Box>
+              <Button
+                type="button"
+                color="info"
+                variant="contained"
+                sx={{ mx: "5px" }}
+                onClick={() => handleDetailClick(params.row.id)}
+              >
+                Detail
+              </Button>
+            </Box>
+            {/* {params.row.numbers.map(
+              (number_info) => ` ${number_info.number}`
+            ) || ""} */}
+          </Box>
+        );
+      },
     },
+    // {
+    //   field: "numbers_detail",
+    //   type: "actions",
+    //   headerName: "Numbers Detail",
+    //   width: 100,
+    //   cellClassName: "actions",
+    //   value: "89234...",
+    //   getActions: ({ id }) => {
+    //     return [
+    //       <GridActionsCellItem
+    //         icon={<DeleteIcon />}
+    //         label="Detail"
+    //         onClick={() => handleDetailClick(id)}
+    //         color="inherit"
+    //       />,
+    //     ];
+    //   },
+    // },
   ];
 
   return (
-    <Box m="20px">
-      <Header title="CARRIERS" subtitle="Create new carrier" />
-      <Box m="40px 0 0 0" height="75vh">
-        <DataGrid rows={carriers} columns={columns} />
+    <>
+      <Modal
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          m="20px"
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 500,
+            bgcolor: "background.paper",
+            border: "1px solid #000",
+            borderRadius: "5px",
+            p: 4,
+          }}
+        >
+          <Box mb="30px">
+            <Typography
+              variant="h4"
+              color={colors.primary[400]}
+              fontWeight="bold"
+              sx={{ mb: "5px" }}
+            >
+              {`Numbers of carrier 
+                ${
+                  carriers &&
+                  carriers.find((carrier) => {
+                    return carrier.id === detailId;
+                  })?.name
+                }`}
+            </Typography>
+          </Box>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <List>
+                {carriers &&
+                  carriers
+                    .find((carrier) => {
+                      return carrier.id === detailId;
+                    })
+                    ?.numbers.map((number_info) => {
+                      return (
+                        <ListItem>
+                          <Typography
+                            variant="h6"
+                            color={colors.greenAccent[400]}
+                          >
+                            {number_info.number}
+                          </Typography>
+                        </ListItem>
+                      );
+                    })}
+              </List>
+            </Grid>
+          </Grid>
+
+          <Box display="flex" justifyContent="end" mt="20px">
+            <Button
+              type="button"
+              color="error"
+              variant="outlined"
+              sx={{ mx: "5px" }}
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Box m="20px">
+        <Header title="CARRIERS" subtitle="Create new carrier" />
+        <Box m="40px 0 0 0" height="75vh">
+          <DataGrid rows={carriers} columns={columns} />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
